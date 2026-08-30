636c255209b8558ce829f6e3f506a773487c0cfc
Insanityyz <161480467+Insanityyz@users.noreply.github.com>
2026-08-30 16:04:15 -0500

    Create StarryPvP.java

---
+++ src/main/java/com/starrypvp/StarryPvP.java
@@ -0,0 +1,108 @@
+package com.starrypvp;
+
+import com.starrypvp.arena.ArenaManager;
+import com.starrypvp.command.PvpCommand;
+import com.starrypvp.data.DataManager;
+import com.starrypvp.gui.SetupGui;
+import com.starrypvp.listener.GameListener;
+import com.starrypvp.match.MatchManager;
+import com.starrypvp.party.PartyManager;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class StarryPvP extends JavaPlugin {
+    private DataManager dataManager;
+    private ArenaManager arenaManager;
+    private PartyManager partyManager;
+    private MatchManager matchManager;
+    private SetupGui setupGui;
+    private YamlConfiguration messages;
+    private File messagesFile;
+
+    public void onEnable() {
+        saveDefaultConfig();
+        saveResourceIfMissing("messages.yml");
+        saveResourceIfMissing("arenas.yml");
+        reloadFiles();
+
+        dataManager = new DataManager(this);
+        arenaManager = new ArenaManager(this);
+        partyManager = new PartyManager();
+        matchManager = new MatchManager(this);
+        setupGui = new SetupGui(this);
+
+        PvpCommand command = new PvpCommand(this);
+        getCommand("pvp").setExecutor(command);
+        getCommand("pvp").setTabCompleter(command);
+
+        getServer().getPluginManager().registerEvents(setupGui, this);
+        getServer().getPluginManager().registerEvents(new GameListener(this), this);
+    }
+
+    public void onDisable() {
+        if (matchManager != null) {
+            matchManager.shutdown();
+        }
+        if (dataManager != null) {
+            dataManager.save();
+        }
+    }
+
+    private void saveResourceIfMissing(String name) {
+        File file = new File(getDataFolder(), name);
+        if (!file.exists()) {
+            saveResource(name, false);
+        }
+    }
+
+    public void reloadFiles() {
+        reloadConfig();
+        messagesFile = new File(getDataFolder(), "messages.yml");
+        messages = YamlConfiguration.loadConfiguration(messagesFile);
+        if (arenaManager != null) {
+            arenaManager.reload();
+        }
+    }
+
+    public String message(String key) {
+        String prefix = messages.getString("prefix", "&8[&dStarryPvP&8] &r");
+        String value = messages.getString(key, "&cMissing message: " + key);
+        return ChatColor.translateAlternateColorCodes('&', prefix + value);
+    }
+
+    public String color(String value) {
+        return ChatColor.translateAlternateColorCodes('&', value);
+    }
+
+    public void saveMessages() {
+        try {
+            messages.save(messagesFile);
+        } catch (IOException exception) {
+            getLogger().warning(exception.getMessage());
+        }
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public ArenaManager getArenaManager() {
+        return arenaManager;
+    }
+
+    public PartyManager getPartyManager() {
+        return partyManager;
+    }
+
+    public MatchManager getMatchManager() {
+        return matchManager;
+    }
+
+    public SetupGui getSetupGui() {
+        return setupGui;
+    }
+}
